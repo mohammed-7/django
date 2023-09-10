295115c5ef160765d022f28053902a6b82d48818
mohammed-7 <khanhuzaif@live.com>
2023-09-10 18:58:10 +0530

    registerd and added models alongside fetching and storing data from frontend

--- crud/app/models.py
+++ crud/app/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     course = models.CharField(max_length=100)
+
+# here the below code is to show the name of the student in admin panel instead of student.object(this called as overwritting method)
+
+    def __str__(self):
+        return self.name
--- crud/app/views.py
+++ crud/app/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Student
 # Create your views here.
 
 
 def index(request):
     return render(request, "app/index.html")
 
+# here we are inserting if loop coz we have to address the data either in post or get, now as we see in terminal that our python code is responsible to get the data from the front end.
+# using return redirect function is to redirect the page back to home after insertion of the data and storing it into database. whenever there's post request we need to redirect it to home page
+# now to store the data we need to import studentmodel by defining object std
+
 
 def create(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        course = request.POST["course"]
+        std = Student(name=name, course=course)
+        std.save()
+        return redirect("/")
+
     return render(request, "app/create.html")
 
 
